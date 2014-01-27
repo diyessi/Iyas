145874994db09fc748b9acce308cfd9d1c790efc
Anton87 <anton_987@libero.it>
2014-01-27 19:20:45 +0100

    Added IllinoisChunker.java

---
+++ src/test/java/qa/qcri/qf/pipeline/InitialProjectRunner.java
@@ -0,0 +1,35 @@
+package qa.qcri.qf.pipeline;
+
+import static org.apache.uima.fit.factory.CollectionReaderFactory.*;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.*;
+import static org.apache.uima.fit.pipeline.SimplePipeline.*;
+
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.component.CasDumpWriter;
+
+import qa.qcri.qf.annotators.IllinoisChunker;
+import de.tudarmstadt.ukp.dkpro.core.io.text.TextReader;
+import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+
+
+public class InitialProjectRunner  {
+	
+	public static void main(String[] args) throws Exception {
+		CollectionReaderDescription cr = createReaderDescription(
+				TextReader.class,
+				TextReader.PARAM_PATH, "src/test/resources/*.txt",
+				TextReader.PARAM_LANGUAGE, "en");
+		
+		AnalysisEngineDescription seg = createEngineDescription(BreakIteratorSegmenter.class);
+		AnalysisEngineDescription tagger = createEngineDescription(OpenNlpPosTagger.class);
+		AnalysisEngineDescription chunker = createEngineDescription(IllinoisChunker.class);
+		AnalysisEngineDescription cc = createPrimitiveDescription(
+				CasDumpWriter.class,
+				CasDumpWriter.PARAM_OUTPUT_FILE, "target/output.txt");
+		
+		runPipeline(cr, seg, tagger, chunker, cc);
+	}
+
+}
