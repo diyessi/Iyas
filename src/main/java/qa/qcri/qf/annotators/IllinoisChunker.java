61f0fa29879228aa8b412a0a3fd45aa28651032c
Massimo Nicosia <m.nicosia@gmail.com>
2014-01-28 12:29:58 +0300

    pom.xml updated for Illinois Chunker libraries

--- src/main/java/qa/qcri/qf/annotators/IllinoisChunker.java
+++ src/main/java/qa/qcri/qf/annotators/IllinoisChunker.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.apache.uima.UimaContext;
 import org.apache.uima.analysis_component.JCasAnnotator_ImplBase;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
-import org.apache.uima.cas.Feature;
 import org.apache.uima.fit.descriptor.TypeCapability;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
