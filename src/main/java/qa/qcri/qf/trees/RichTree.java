b95a83b0d61d9d48e964d8850d0120be3c99d066
Massimo Nicosia <m.nicosia@gmail.com>
2014-01-28 10:46:45 +0300

    RichTree javadoc

--- src/main/java/qa/qcri/qf/trees/RichTree.java
+++ src/main/java/qa/qcri/qf/trees/RichTree.java
@@ -8,36 +8,54 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.chunk.Chunk;
 
 public class RichTree {
-	
+
+	public static final String ROOT_LABEL = "ROOT";
+	public static final String SENTENCE_LABEL = "S";
+
+	/**
+	 * Builds a POS+CHUNK tree from an annotated CAS
+	 * 
+	 * The CAS must contain sentence boundaries, tokens,
+	 * POStags and chunks
+	 * 
+	 * A POS+CHUNK tree is a tree with two salient layer.
+	 * The bottom layer is made by tokens with their POStags.
+	 * These nodes are grouped by chunk nodes.
+	 * 
+	 * @param cas the UIMA JCas
+	 * @return the POS+CHUNK tree, as a TokenTree
+	 * 
+	 * @see TokenTree
+	 */
 	public static TokenTree getPosChunkTree(JCas cas) {
-		TokenTree root = new TokenTree();	
-		root.setValue("ROOT");
-		
+		TokenTree root = new TokenTree();
+		root.setValue(ROOT_LABEL);
+
 		for (Sentence sentence : JCasUtil.select(cas, Sentence.class)) {
 			RichNode sentenceNode = new BaseRichNode();
-			sentenceNode.setValue("S");
+			sentenceNode.setValue(SENTENCE_LABEL);
 
-			for(Chunk chunk : JCasUtil.selectCovered(cas, Chunk.class, sentence)) {
+			for (Chunk chunk : JCasUtil.selectCovered(cas, Chunk.class, sentence)) {
 				RichNode chunkNode = new RichChunkNode(chunk);
-				for(Token token : JCasUtil.selectCovered(cas, Token.class, chunk)) {
+				for (Token token : JCasUtil.selectCovered(cas, Token.class, chunk)) {
 					RichNode posNode = new BaseRichNode();
 					posNode.setValue(token.getPos().getPosValue());
-					
+
 					RichTokenNode tokenNode = new RichTokenNode(token);
-					
+
 					posNode.addChild(tokenNode);
 					chunkNode.addChild(posNode);
-					
+
 					root.addToken(tokenNode);
 				}
 				sentenceNode.addChild(chunkNode);
 			}
-			
+
 			root.addChild(sentenceNode);
 		}
-		
+
 		return root;
-		
+
 	}
-	
+
 }
