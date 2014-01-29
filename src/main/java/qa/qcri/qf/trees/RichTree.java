03a9898c8c1ea043a246e623f460191a6e49ad5d
Massimo Nicosia <m.nicosia@gmail.com>
2014-01-29 15:30:58 +0300

    Node, serializer and test for Constituency trees

--- src/main/java/qa/qcri/qf/trees/RichTree.java
+++ src/main/java/qa/qcri/qf/trees/RichTree.java
@@ -1,11 +1,16 @@
 package qa.qcri.qf.trees;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.chunk.Chunk;
+import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.constituent.Constituent;
 
 /**
  * 
@@ -17,13 +22,10 @@ public class RichTree {
 	public static final String SENTENCE_LABEL = "S";
 
 	/**
-	 * Builds a POS+CHUNK tree from an annotated CAS
-	 * 
-	 * The CAS must contain sentence boundaries, tokens, POStags and chunks
-	 * 
-	 * A POS+CHUNK tree is a tree with two salient layer. The bottom layer is
-	 * made by tokens with their POStags. These nodes are grouped by chunk
-	 * nodes.
+	 * Builds a POS+CHUNK tree from an annotated CAS. The CAS must contain
+	 * sentence boundaries, tokens, POStags and chunks A POS+CHUNK tree is a
+	 * tree with two salient layer. The bottom layer is made by tokens with
+	 * their POStags. These nodes are grouped by chunk nodes.
 	 * 
 	 * @param cas
 	 *            the UIMA JCas
@@ -61,7 +63,78 @@ public class RichTree {
 		}
 
 		return root;
-
 	}
 
+	/**
+	 * Builds a Constituency tree from an annotated CAS The CAS must contain
+	 * sentence boundaries, tokens, POStags and constituents
+	 * 
+	 * @param cas
+	 *            the UIMA JCas
+	 * @return the Constituency tree, as a TokenTree
+	 * 
+	 * @see TokenTree
+	 */
+	public static TokenTree getConstituencyTree(JCas cas) {
+		TokenTree root = new TokenTree();
+		root.setValue(ROOT_LABEL);
+
+		List<Constituent> roots = new ArrayList<>();
+
+		for (Constituent constituent : JCasUtil.select(cas, Constituent.class)) {
+			if (constituent.getConstituentType().equals(ROOT_LABEL)) {
+				roots.add(constituent);
+			}
+		}
+
+		for (Constituent node : roots) {
+			RichNode subTree = getConstituencySubTree(node, root);
+			for (RichNode sentenceNode : subTree.getChildren()) {
+				root.addChild(sentenceNode);
+			}
+		}
+
+		return root;
+	}
+
+	/**
+	 * Recursive method for producing constituency trees out of a node
+	 * 
+	 * @param subTreeRoot
+	 *            the root node of the subtree
+	 * @param root
+	 *            the root of the tree
+	 * @return a serialized constituency subtree
+	 */
+	private static RichNode getConstituencySubTree(Constituent subTreeRoot,
+			TokenTree root) {
+		RichNode subTree = new RichConstituentNode(subTreeRoot);
+
+		Collection<Constituent> constituents = JCasUtil.select(
+				subTreeRoot.getChildren(), Constituent.class);
+
+		for (Constituent constituent : constituents) {
+			RichNode constituentNode = getConstituencySubTree(constituent, root);
+			subTree.addChild(constituentNode);
+		}
+
+		if (constituents.size() == 0) {
+			Collection<Token> tokens = JCasUtil.select(
+					subTreeRoot.getChildren(), Token.class);
+
+			for (Token token : tokens) {
+				RichNode posNode = new BaseRichNode();
+				posNode.setValue(token.getPos().getPosValue());
+
+				RichTokenNode tokenNode = new RichTokenNode(token);
+
+				posNode.addChild(tokenNode);
+				subTree.addChild(posNode);
+
+				root.addToken(tokenNode);
+			}
+		}
+
+		return subTree;
+	}
 }
---
+++ src/test/java/qa/qcri/qf/pipeline/ConstituencyTreeTest.java
@@ -0,0 +1,50 @@
+package qa.qcri.qf.pipeline;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import org.apache.uima.UIMAException;
+import org.apache.uima.fit.factory.JCasFactory;
+import org.apache.uima.jcas.JCas;
+import org.junit.Assert;
+import org.junit.Test;
+
+import qa.qcri.qf.pipeline.retrieval.SimpleContent;
+import qa.qcri.qf.pipeline.serialization.UIMAFilePersistence;
+import qa.qcri.qf.trees.RichTree;
+import qa.qcri.qf.trees.TokenTree;
+import qa.qcri.qf.trees.TreeSerializer;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordLemmatizer;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordParser;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
+
+public class ConstituencyTreeTest {
+
+	@Test
+	public void test() throws UIMAException {
+		Analyzer ae = instantiateAnalyzer();
+		JCas cas = JCasFactory.createJCas();
+
+		ae.analyze(cas, new SimpleContent("constituency-test",
+				"My dog also likes eating sausage. What about your cat?"));
+
+		TokenTree tree = RichTree.getConstituencyTree(cas);
+		TreeSerializer ts = new TreeSerializer();
+
+		Assert.assertEquals(
+				"(ROOT (S (NP (PRP$ (My))(NN (dog)))(ADVP (RB (also)))(VP (VP (NP (NN (sausage))))))(FRAG (WHNP (WHNP (WP (What)))(PP (NP (PRP$ (your))(NN (cat)))))))",
+				ts.serializeTree(tree, ""));
+	}
+
+	private Analyzer instantiateAnalyzer() throws UIMAException {
+		Analyzer ae = new Analyzer(new UIMAFilePersistence("CASes"));
+
+		ae.addAEDesc(createEngineDescription(StanfordSegmenter.class))
+				.addAEDesc(createEngineDescription(StanfordPosTagger.class))
+				.addAEDesc(createEngineDescription(StanfordLemmatizer.class))
+				.addAEDesc(createEngineDescription(StanfordParser.class));
+
+		return ae;
+	}
+
+}
