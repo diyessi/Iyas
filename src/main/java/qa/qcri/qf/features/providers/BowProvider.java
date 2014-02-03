52485d9d061e74f12849f6182fd1d729d4ce1c18
Anton87 <anton_987@libero.it>
2014-02-03 17:34:28 +0100

    Added BowProvider

---
+++ src/main/java/qa/qcri/qf/features/providers/BowProvider.java
@@ -0,0 +1,65 @@
+package qa.qcri.qf.features.providers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+
+import cc.mallet.types.Alphabet;
+import cc.mallet.types.FeatureSequence;
+import cc.mallet.types.FeatureVector;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.ngrams.util.NGramStringIterable;
+
+import svmlighttk.SVMVector;
+
+public class BowProvider {	
+
+	private final static int MIN_N = 1;
+	private final static int MAX_N = 1;
+	
+	private Alphabet featureDict;
+	private int minN;
+	private int maxN;
+	
+	public BowProvider() {
+		this(MIN_N, MAX_N);
+	}
+	//private TokenTextGetter tGetter;
+	
+	public BowProvider(int minN, int maxN) {
+		this.minN = minN;
+		this.maxN = maxN;
+		this.featureDict = new Alphabet();
+	}
+	
+	public SVMVector getSVMVector(JCas cas, Map<String, Double> idf) {
+		SVMVector vec = new SVMVector(getFeatureVector(cas));
+		return vec;
+	}
+	
+	public FeatureVector getFeatureVector(JCas cas) {
+		FeatureVector fv = new FeatureVector(getNGramFeatureSeqFromCas(cas));
+		return fv;
+	}
+	
+	public FeatureSequence getNGramFeatureSeqFromCas(JCas cas) {
+		FeatureSequence featureSeq = new FeatureSequence(featureDict);
+		
+		List<String> tokens = new ArrayList<>();
+		for (Token token : JCasUtil.select(cas, Token.class)) {
+			String text = token.getCoveredText();
+			if (text != null) {
+				tokens.add(text);
+			}
+		}
+		NGramStringIterable ngramsStringIt = new NGramStringIterable(tokens, minN, maxN);
+		for (String ngram : ngramsStringIt) { 
+			featureSeq.add(ngram);			
+		}		
+		return featureSeq;
+	}
+
+}
