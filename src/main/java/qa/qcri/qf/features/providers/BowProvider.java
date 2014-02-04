db11a36f899ea217dd62e69e62f94ebf162fcf07
Anton87 <anton_987@libero.it>
2014-02-04 14:40:26 +0100

    Added BowProvider.java

--- src/main/java/qa/qcri/qf/features/providers/BowProvider.java
+++ src/main/java/qa/qcri/qf/features/providers/BowProvider.java
@@ -7,32 +7,40 @@ import java.util.Map;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 
+import qa.qcri.qf.trees.nodes.RichTokenNode;
+import qa.qcri.qf.trees.nodes.TokenTextGetterFactory;
 import cc.mallet.types.Alphabet;
 import cc.mallet.types.FeatureSequence;
 import cc.mallet.types.FeatureVector;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.ngrams.util.NGramStringIterable;
-
 import svmlighttk.SVMVector;
 
 public class BowProvider {	
 
-	private final static int MIN_N = 1;
-	private final static int MAX_N = 1;
+	private final static int DEFAULT_MIN_N = 1;
+	private final static int DEFAULT_MAX_N = 1;
+	private static final String DEFAULT_TOKEN_TEXT_TYPE = TokenTextGetterFactory.LEMMA;
 	
 	private Alphabet featureDict;
 	private int minN;
 	private int maxN;
+	private String parametersList;
 	
 	public BowProvider() {
-		this(MIN_N, MAX_N);
+		this(DEFAULT_TOKEN_TEXT_TYPE, DEFAULT_MIN_N, DEFAULT_MAX_N);
 	}
-	//private TokenTextGetter tGetter;
 	
-	public BowProvider(int minN, int maxN) {
+	public BowProvider(String parametersList) {
+		this(parametersList, DEFAULT_MIN_N, DEFAULT_MAX_N);
+	}
+	
+	public BowProvider(String parametersList, int minN, int maxN) {
 		this.minN = minN;
 		this.maxN = maxN;
 		this.featureDict = new Alphabet();
+		this.parametersList = parametersList;
+		//this.tokenTextGetter = TokenTextGetterFactory.getTokenTextGetter(tokenTextType);
 	}
 	
 	public SVMVector getSVMVector(JCas cas, Map<String, Double> idf) {
@@ -50,7 +58,7 @@ public class BowProvider {
 		
 		List<String> tokens = new ArrayList<>();
 		for (Token token : JCasUtil.select(cas, Token.class)) {
-			String text = token.getCoveredText();
+			String text = new RichTokenNode(token).getRepresentation(parametersList);
 			if (text != null) {
 				tokens.add(text);
 			}
