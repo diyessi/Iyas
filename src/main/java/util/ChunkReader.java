7735484e5fb3b7eb5c557ffb70111ccef785ebcf
Massimo Nicosia <m.nicosia@gmail.com>
2014-02-05 09:12:33 +0300

    Removed method copyList. Lists.newArrayList adopted

--- src/main/java/util/ChunkReader.java
+++ src/main/java/util/ChunkReader.java
@@ -7,40 +7,45 @@ import java.util.List;
 import qa.qcri.qf.fileutil.ReadFile;
 
 import com.google.common.base.Function;
+import com.google.common.collect.Lists;
 
 /**
  * 
- * Reads from a file blocks of sequential lines having a common characteristic 
+ * Reads from a file blocks of sequential lines having a common characteristic
  */
 public class ChunkReader implements Iterable<List<String>> {
 
 	private ReadFile in;
-	
+
 	private List<String> linesBuffer = new ArrayList<>();
-	
+
 	private String previousExtractedItem = null;
-	
+
 	private Function<String, String> groupingItemExtraction;
-	
+
 	/**
-	 * Instantiates the reader with the specified extraction function.
-	 * This function extracts a text feature from a line of text, which is used
-	 * to select lines belonging to the same block.
-	 * @param path the path of the file to read
-	 * @param groupingItemExtraction the extraction function
+	 * Instantiates the reader with the specified extraction function. This
+	 * function extracts a text feature from a line of text, which is used to
+	 * select lines belonging to the same block.
+	 * 
+	 * @param path
+	 *            the path of the file to read
+	 * @param groupingItemExtraction
+	 *            the extraction function
 	 */
-	public ChunkReader(String path, Function<String, String> groupingItemExtraction) {
+	public ChunkReader(String path,
+			Function<String, String> groupingItemExtraction) {
 		this.in = new ReadFile(path);
 		this.groupingItemExtraction = groupingItemExtraction;
 	}
-	
+
 	@Override
 	public Iterator<List<String>> iterator() {
 		Iterator<List<String>> iterator = new Iterator<List<String>>() {
 
 			@Override
 			public boolean hasNext() {
-				if(linesBuffer.isEmpty() && !in.hasNextLine()) {
+				if (linesBuffer.isEmpty() && !in.hasNextLine()) {
 					return false;
 				} else {
 					return true;
@@ -49,54 +54,48 @@ public class ChunkReader implements Iterable<List<String>> {
 
 			@Override
 			public List<String> next() {
-				
+
 				List<String> returnList = new ArrayList<>();
-				
-				if(!linesBuffer.isEmpty() && !in.hasNextLine()) {
-					returnList = copyList(linesBuffer);
+
+				if (!linesBuffer.isEmpty() && !in.hasNextLine()) {
+					returnList = Lists.newArrayList(linesBuffer);
 					linesBuffer.clear();
 					return returnList;
 				}
-				
-				while(in.hasNextLine()) {
+
+				while (in.hasNextLine()) {
 					String currentLine = in.nextLine().trim();
-					
-					if(currentLine.isEmpty()) continue;
-					
-					String extractedItem = groupingItemExtraction.apply(currentLine);
-					if(previousExtractedItem == null) {
+
+					if (currentLine.isEmpty())
+						continue;
+
+					String extractedItem = groupingItemExtraction
+							.apply(currentLine);
+					if (previousExtractedItem == null) {
 						previousExtractedItem = extractedItem;
 						linesBuffer.add(currentLine);
 					} else {
-						if(previousExtractedItem.equals(extractedItem)) {
+						if (previousExtractedItem.equals(extractedItem)) {
 							linesBuffer.add(currentLine);
-						} else {						
-							returnList = copyList(linesBuffer);
+						} else {
+							returnList = Lists.newArrayList(linesBuffer);
 							previousExtractedItem = extractedItem;
 							linesBuffer.clear();
 							linesBuffer.add(currentLine);
-							
+
 							return returnList;
 						}
 					}
 				}
-				
-				if(linesBuffer.isEmpty()) {
+
+				if (linesBuffer.isEmpty()) {
 					return new ArrayList<String>();
 				} else {
-					returnList = copyList(linesBuffer);
+					returnList = Lists.newArrayList(linesBuffer);
 					linesBuffer.clear();
 					return returnList;
 				}
-				
-			}
-			
-			private List<String> copyList(List<String> list) {
-				List<String> copy = new ArrayList<>();
-				for(String item : list) {
-					copy.add(item);
-				}
-				return copy;
+
 			}
 
 			@Override
@@ -104,7 +103,7 @@ public class ChunkReader implements Iterable<List<String>> {
 				throw new UnsupportedOperationException();
 			}
 		};
-		
+
 		return iterator;
 	}
 
