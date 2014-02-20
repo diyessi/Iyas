804b4d48fd41d7a81c8239ac652348536e40e608
Massimo Nicosia <m.nicosia@gmail.com>
2014-02-20 12:28:51 +0300

    Installing the Arabic Pos Tagger and CRFPP

--- resources/install-resources.sh
+++ resources/install-resources.sh
@@ -1,1 +1,16 @@
 tar -xzf limosine.tar.gz
+tar -xzf APOSTLib.tgz
+
+tar -xzf CRF++-0.58.tar.gz
+cd CRF++-0.58/
+./configure
+make
+
+# make sure that in CRF++-0.58/java/Makefile the jni.h file is correctly specified
+cd java
+make
+
+cd ../../
+
+cp CRF++-0.58/java/CRFPP.jar ../lib/CRFPP.jar
+cp CRF++-0.58/java/libCRFPP.so ../resources/ArabicPOSTLib/data/libCRFPP.so
