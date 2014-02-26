92aa50c3d9e6d2423cde7c37791ca4209c2b0621
Massimo Nicosia <m.nicosia@gmail.com>
2014-02-26 15:17:42 +0300

    Fixed documentation and cosmetic edits

--- src/main/java/qa/qcri/qf/trees/nodes/RichDependencyNode.java
+++ src/main/java/qa/qcri/qf/trees/nodes/RichDependencyNode.java
@@ -4,20 +4,20 @@ import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.dependency.Dependency;
 
 /**
  * 
- * The RichDependencyNode class wraps a Dependency object a datatype from the
- *
+ * The RichDependencyNode class wraps a Dependency object, a datatype from the
+ * DKPro typesystem
  */
 public class RichDependencyNode extends BaseRichNode {
-	
+
 	private Dependency dependency;
-	
+
 	public RichDependencyNode(Dependency dependency) {
 		super();
 		this.dependency = dependency;
 		this.metadata.put(RichNode.TYPE_KEY, RichNode.TYPE_DEPENDENCY_NODE);
 		this.value = dependency.getDependencyType();
 	}
-	
+
 	/**
 	 * 
 	 * @return the DKPro Dependency object
