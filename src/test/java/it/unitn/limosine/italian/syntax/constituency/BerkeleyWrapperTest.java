3272c658f1a1ff7728b78313d3e1d7d244944235
Anton87 <anton_987@libero.it>
2014-04-11 14:43:19 +0200

    Modified BerkeleyWrapperTest.java

--- src/test/java/it/unitn/limosine/italian/syntax/constituency/BerkeleyWrapperTest.java
+++ src/test/java/it/unitn/limosine/italian/syntax/constituency/BerkeleyWrapperTest.java
@@ -7,9 +7,7 @@ import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
 import org.junit.Before;
 import org.junit.Test;
-import org.uimafit.util.JCasUtil;
 
-import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import qa.qcri.qf.pipeline.Analyzer;
 import qa.qcri.qf.pipeline.retrieval.SimpleContent;
 import qa.qcri.qf.trees.RichTree;
@@ -21,12 +19,13 @@ public class BerkeleyWrapperTest {
 	
 	private JCas cas = null;
 	
-	String msg = "Renault ha una completa gamma di veicoli elettrici.";
-	private String expectedParse = "(ROOT (S (NP (SPN (Renault)))(VXI (VI (ha)))(NP (RS (una))(ADJP (AS (completa)))(SS (gamma))(PX (E (di))(NP (SP (veicoli))(ADJP (AP (elettrici)))))))(XPS (.)))";
+	String sent = "Renault ha una completa gamma di veicoli elettrici.";
+	
+	private String expectedSentParse = "(ROOT (S (NP (SPN (Renault)))(VXI (VI (ha)))(NP (RS (una))(ADJP (AS (completa)))(SS (gamma))(PX (E (di))(NP (SP (veicoli))(ADJP (AP (elettrici)))))))(XPS (.)))";
 	
 	@Before
 	public void setUp() throws Exception { 
-		SimpleContent content = new SimpleContent("test", msg);
+		SimpleContent content = new SimpleContent("test", sent);
 		cas = initCas(content);
 	}	
 
@@ -48,9 +47,7 @@ public class BerkeleyWrapperTest {
 		TreeSerializer ts = new TreeSerializer();
 		String serializedConstTree = ts.serializeTree(constituencyTree, RichNode.OUTPUT_PAR_TOKEN);
 		
-		//System.out.println("expected: " + expectedParse);
-		//System.out.println("serialzd: " + serializedConstTree);
-		assertEquals(expectedParse, serializedConstTree);
+		assertEquals(expectedSentParse, serializedConstTree);
 	}
 
 }
