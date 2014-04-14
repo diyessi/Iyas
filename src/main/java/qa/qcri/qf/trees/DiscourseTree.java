a976c394f591f3a59dd6c135026712f9078e0a6f
Massimo Nicosia <m.nicosia@gmail.com>
2014-04-14 16:14:27 +0300

    JavaDoc

--- src/main/java/qa/qcri/qf/trees/DiscourseTree.java
+++ src/main/java/qa/qcri/qf/trees/DiscourseTree.java
@@ -17,42 +17,63 @@ import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 import de.tudarmstadt.ukp.dkpro.core.api.syntax.type.chunk.Chunk;
 
 public class DiscourseTree {
-	
+
 	public static final String ROOT_LABEL = "ROOT";
 	public static final String SENTENCE_LABEL = "S";
-	
-	public static TokenTree getDiscourseTree(JCas cas) {	
+
+	/**
+	 * Builds a tree with only Discourse nodes from an annotated CAS. The CAS
+	 * must contain Discourse annotations. All the nodes are RichDiscourseNode
+	 * and can be cast safely
+	 * 
+	 * @param cas
+	 *            the UIMA JCas
+	 * @return the Discourse tree, as a TokenTree, but actually it has no tokens
+	 * 
+	 * @see TokenTree
+	 */
+	public static TokenTree getDiscourseTree(JCas cas) {
 		TokenTree root = new TokenTree();
 		root.setValue(ROOT_LABEL);
-		
+
 		List<Discourse> roots = new ArrayList<>();
 
 		for (Discourse discourse : JCasUtil.select(cas, Discourse.class)) {
-			if (discourse.getValue().endsWith("_ROOT")) { 
+			if (discourse.getValue().endsWith("_ROOT")) {
 				roots.add(discourse);
 			}
 		}
-		
-		for(Discourse node : roots) {
+
+		for (Discourse node : roots) {
 			RichNode rootSubTree = getDiscourseSubTree(node, root);
 			root.addChild(rootSubTree);
 		}
-		
+
 		return root;
 	}
-	
+
+	/**
+	 * Builds a tree with Discourse nodes and tokens from an annotated CAS. The
+	 * CAS must contain Discourse and Token annotations.
+	 * 
+	 * @param cas
+	 *            the UIMA JCas
+	 * @return the Discourse tree, as a TokenTree
+	 * 
+	 * @see TokenTree
+	 */
 	public static TokenTree getDiscourseTreeWithTokens(JCas cas) {
 		TokenTree tree = getDiscourseTree(cas);
-		
+
 		List<RichNode> discourseLeaves = TreeUtil.getLeaves(tree);
-		
-		for(RichNode node : discourseLeaves) {
+
+		for (RichNode node : discourseLeaves) {
 			RichDiscourseNode leaf = (RichDiscourseNode) node;
 			Discourse discourse = leaf.getDiscourse();
-			
-			for(Token token : JCasUtil.selectCovered(Token.class, discourse)) {
-				RichNode posNode = new BaseRichNode().setValue(token
-						.getPos().getPosValue());
+
+			for (Token token : JCasUtil.selectCovered(Token.class, discourse)) {
+				RichNode posNode = new BaseRichNode().setValue(token.getPos()
+						.getPosValue());
 
 				RichTokenNode tokenNode = new RichTokenNode(token);
 
@@ -61,51 +82,70 @@ public class DiscourseTree {
 				tree.addToken(tokenNode);
 			}
 		}
-		
-		return tree;
-	}
-	
-	public static TokenTree getDiscourseTreeWithTokensAndChunks(JCas cas) {
-		TokenTree tree = getDiscourseTree(cas);
-		
-		List<RichNode> discourseLeaves = TreeUtil.getLeaves(tree);
-		
-		for(RichNode node : discourseLeaves) {
-			RichDiscourseNode leaf = (RichDiscourseNode) node;
-			Discourse discourse = leaf.getDiscourse();
-			
-			for(Chunk chunk : JCasUtil.selectCovered(Chunk.class, discourse)) {
-				
-				RichNode chunkNode = new RichChunkNode(chunk);
-			
-				for(Token token : JCasUtil.selectCovered(Token.class, chunk)) {
-					RichNode posNode = new BaseRichNode().setValue(token
-							.getPos().getPosValue());
-	
-					RichTokenNode tokenNode = new RichTokenNode(token);
-	
-					chunkNode.addChild(posNode.addChild(tokenNode));
-	
-					tree.addToken(tokenNode);
-				}
-				
-				node.addChild(chunkNode);
-			}
-		}
-		
+
 		return tree;
 	}
 
-	private static RichNode getDiscourseSubTree(Discourse subTreeRoot, TokenTree root) {
+	/**
+	 * Builds a tree with Discourse nodes, chunks and tokens from an annotated
+	 * CAS. The CAS must contain Discourse, Chunk and Token annotations.
+	 * 
+	 * @param cas
+	 *            the UIMA JCas
+	 * @return the Discourse tree, as a TokenTree
+	 * 
+	 * @see TokenTree
+	 */
+	public static TokenTree getDiscourseTreeWithTokensAndChunks(JCas cas) {
+		TokenTree tree = getDiscourseTree(cas);
+
+		List<RichNode> discourseLeaves = TreeUtil.getLeaves(tree);
+
+		for (RichNode node : discourseLeaves) {
+			RichDiscourseNode leaf = (RichDiscourseNode) node;
+			Discourse discourse = leaf.getDiscourse();
+
+			for (Chunk chunk : JCasUtil.selectCovered(Chunk.class, discourse)) {
+				RichNode chunkNode = new RichChunkNode(chunk);
+
+				for (Token token : JCasUtil.selectCovered(Token.class, chunk)) {
+					RichNode posNode = new BaseRichNode().setValue(token
+							.getPos().getPosValue());
+
+					RichTokenNode tokenNode = new RichTokenNode(token);
+
+					chunkNode.addChild(posNode.addChild(tokenNode));
+
+					tree.addToken(tokenNode);
+				}
+
+				node.addChild(chunkNode);
+			}
+		}
+
+		return tree;
+	}
+
+	/**
+	 * Recursive method for producing discourse trees out of a node
+	 * 
+	 * @param subTreeRoot
+	 *            the root node of the subtree
+	 * @param root
+	 *            the root of the tree
+	 * @return a serialized discourse subtree
+	 */
+	private static RichNode getDiscourseSubTree(Discourse subTreeRoot,
+			TokenTree root) {
 		RichNode subTree = new RichDiscourseNode(subTreeRoot);
-		
+
 		Collection<Discourse> discourses = JCasUtil.select(
 				subTreeRoot.getChildren(), Discourse.class);
 
 		for (Discourse discourse : discourses) {
 			subTree.addChild(getDiscourseSubTree(discourse, root));
 		}
-		
+
 		return subTree;
 	}
 }
