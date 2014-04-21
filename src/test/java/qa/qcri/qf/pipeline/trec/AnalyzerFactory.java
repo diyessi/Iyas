ac0bb5426f856b28fe1e133399897259666a48e6
Massimo Nicosia <m.nicosia@gmail.com>
2014-04-21 16:01:57 +0300

    Analysis engines are reused to build multiple pipeline: in this case, an additional Question Analysis pipeline

--- src/test/java/qa/qcri/qf/pipeline/trec/AnalyzerFactory.java
+++ src/test/java/qa/qcri/qf/pipeline/trec/AnalyzerFactory.java
@@ -5,61 +5,113 @@ import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDesc
 import java.io.IOException;
 
 import org.apache.uima.UIMAException;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
 
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordLemmatizer;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordNamedEntityRecognizer;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordParser;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordPosTagger;
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
 import qa.qcri.qf.annotators.IllinoisChunker;
+import qa.qcri.qf.annotators.QuestionClassifier;
+import qa.qcri.qf.annotators.QuestionFocusClassifier;
 import qa.qcri.qf.pipeline.Analyzer;
 import qa.qcri.qf.pipeline.serialization.UIMAPersistence;
 
 public abstract class AnalyzerFactory {
-	
-	public static Analyzer newTrecPipeline(String lang, UIMAPersistence persistence) 
-		throws UIMAException {
-		if (lang == null) { 
-			throw new NullPointerException("lang is null");
+
+	public static final String QUESTION_ANALYSIS = "QUESTION_ANALYSIS";
+
+	public static Analyzer newTrecPipeline(String lang,
+			UIMAPersistence persistence) throws UIMAException {
+
+		if (lang == null) {
+			throw new NullPointerException("The String parameter lang is null");
 		}
-		if (persistence == null) { 
-			throw new NullPointerException("persistence is null");
+
+		if (persistence == null) {
+			throw new NullPointerException(
+					"The UIMAPersistence parameter persistence is null");
 		}
-		
-		Analyzer analyzer = null;		
-		if (lang.equals("en")) {
-			analyzer = newTrecPipelineEnAnalyzer(persistence);			
-		} else if (lang.equals("it")) {
+
+		Analyzer analyzer = null;
+
+		switch (lang) {
+		case "en":
+			analyzer = newTrecPipelineEnAnalyzer(persistence);
+			break;
+		case "it":
 			analyzer = newTrecPipelineItAnalyzer(persistence);
+			break;
+		default:
+			analyzer = newTrecPipelineEnAnalyzer(persistence);
+			break;
 		}
-				
+
 		return analyzer;
 	}
-	
-	private static Analyzer newTrecPipelineEnAnalyzer(UIMAPersistence persistence) 
-		throws UIMAException {
+
+	private static Analyzer newTrecPipelineEnAnalyzer(
+			UIMAPersistence persistence) throws UIMAException {
 		assert persistence != null;
-		
+
 		Analyzer ae = new Analyzer(persistence);
-		
-		ae.addAEDesc(createEngineDescription(StanfordSegmenter.class))
-		  .addAEDesc(createEngineDescription(StanfordPosTagger.class))
-		  .addAEDesc(createEngineDescription(StanfordLemmatizer.class))
-		  .addAEDesc(createEngineDescription(IllinoisChunker.class));
-		  
+
+		AnalysisEngine stanfordSegmenter = AnalysisEngineFactory
+				.createEngine(createEngineDescription(StanfordSegmenter.class));
+
+		AnalysisEngine stanfordPosTagger = AnalysisEngineFactory
+				.createEngine(createEngineDescription(StanfordPosTagger.class));
+
+		AnalysisEngine stanfordLemmatizer = AnalysisEngineFactory
+				.createEngine(createEngineDescription(StanfordLemmatizer.class));
+
+		AnalysisEngine illinoisChunker = AnalysisEngineFactory
+				.createEngine(createEngineDescription(IllinoisChunker.class));
+
+		AnalysisEngine stanfordParser = AnalysisEngineFactory
+				.createEngine(createEngineDescription(StanfordParser.class));
+
+		AnalysisEngine stanfordNamedEntityRecognizer = AnalysisEngineFactory
+				.createEngine(createEngineDescription(StanfordNamedEntityRecognizer.class));
+
+		AnalysisEngine questionClassifier = AnalysisEngineFactory
+				.createEngine(createEngineDescription(QuestionClassifier.class));
+
+		AnalysisEngine questionFocusClassifier = AnalysisEngineFactory
+				.createEngine(createEngineDescription(QuestionFocusClassifier.class));
+
+		ae.addAE(stanfordSegmenter)
+			.addAE(stanfordPosTagger)
+			.addAE(stanfordLemmatizer)
+			.addAE(illinoisChunker)
+			.addAE(stanfordNamedEntityRecognizer);
+
+		ae.addAE(stanfordSegmenter, QUESTION_ANALYSIS)
+			.addAE(stanfordPosTagger, QUESTION_ANALYSIS)
+			.addAE(stanfordLemmatizer, QUESTION_ANALYSIS)
+			.addAE(illinoisChunker, QUESTION_ANALYSIS)
+			.addAE(stanfordNamedEntityRecognizer, QUESTION_ANALYSIS)
+			.addAE(stanfordParser, QUESTION_ANALYSIS)
+			.addAE(questionClassifier, QUESTION_ANALYSIS)
+			.addAE(questionFocusClassifier, QUESTION_ANALYSIS);
+
 		return ae;
 	}
-	
-	private static Analyzer newTrecPipelineItAnalyzer(UIMAPersistence persistence) 
-		throws UIMAException {
+
+	private static Analyzer newTrecPipelineItAnalyzer(
+			UIMAPersistence persistence) throws UIMAException {
 		assert persistence != null;
-		
+
 		Analyzer ae = new Analyzer(persistence);
-		
+
 		try {
 			ae.addAEDesc(createEngineDescription("desc/Limosine/TextProFixAllInOneDescriptor"));
-		} catch (IOException e) { 
+		} catch (IOException e) {
 			throw new UIMAException(e);
 		}
-		
+
 		return ae;
 	}
 
