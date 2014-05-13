673128f4fc89f4fa0739fe7291962ce829953c4f
Massimo Nicosia <m.nicosia@gmail.com>
2014-05-13 12:16:01 +0300

    Updated TreeUtil

--- src/main/java/qa/qcri/qf/trees/TreeUtil.java
+++ src/main/java/qa/qcri/qf/trees/TreeUtil.java
@@ -1,7 +1,9 @@
 package qa.qcri.qf.trees;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import qa.qcri.qf.trees.nodes.RichNode;
 
@@ -14,7 +16,7 @@ public class TreeUtil {
 	 * 
 	 * @param tree
 	 *            the tree to traverse
-	 * @return a list of leaves in the order right-to-left
+	 * @return a list of nodes
 	 */
 	public static List<RichNode> getNodes(RichNode tree) {
 		List<RichNode> nodes = new ArrayList<>();
@@ -72,28 +74,81 @@ public class TreeUtil {
 	 * @return the leaves of the tree
 	 */
 	public static List<RichNode> getLeaves(RichNode tree) {
-		return TreeUtil.getNodesWithFilter(tree, new Function<RichNode, Boolean>() {
-			@Override
-			public Boolean apply(RichNode node) {
-				return node.isLeaf();
-			}
-		});
+		return TreeUtil.getNodesWithFilter(tree,
+				new Function<RichNode, Boolean>() {
+					@Override
+					public Boolean apply(RichNode node) {
+						return node.isLeaf();
+					}
+				});
 	}
-	
+
 	/**
 	 * Returns the nodes of the tree which are not leaves
 	 * 
 	 * @param tree
 	 *            the tree to traverse
-	 * @return the node which are not leaves of the tree
+	 * @return the nodes of the tree which are not leaves
 	 */
 	public static List<RichNode> getNonLeaves(RichNode tree) {
-		return TreeUtil.getNodesWithFilter(tree, new Function<RichNode, Boolean>() {
-			@Override
-			public Boolean apply(RichNode node) {
-				return !node.isLeaf();
+		return TreeUtil.getNodesWithFilter(tree,
+				new Function<RichNode, Boolean>() {
+					@Override
+					public Boolean apply(RichNode node) {
+						return !node.isLeaf();
+					}
+				});
+	}
+
+	/**
+	 * Returns the parents of the leaves
+	 * 
+	 * @param tree
+	 *            the tree to traverse
+	 * @return the parent nodes of the leaves
+	 */
+	public static List<RichNode> getLeavesParents(RichNode tree) {
+		List<RichNode> leavesParents = new ArrayList<>();
+
+		List<RichNode> leaves = TreeUtil.getLeaves(tree);
+		Set<RichNode> parents = new HashSet<>();
+
+		for (RichNode leaf : leaves) {
+			RichNode parent = leaf.getParent();
+			if (!parents.contains(parent)) {
+				leavesParents.add(parent);
+				parents.add(parent);
 			}
-		});
+		}
+
+		return leavesParents;
+	}
+	
+	/**
+	 * Returns the grandparents of the leaves
+	 * 
+	 * @param tree
+	 *            the tree to traverse
+	 * @return the grandparent nodes of the leaves
+	 */
+	public static List<RichNode> getLeavesGrandParents(RichNode tree) {
+		List<RichNode> leavesGrandParents = new ArrayList<>();
+
+		List<RichNode> leaves = TreeUtil.getLeaves(tree);
+		Set<RichNode> grandParents = new HashSet<>();
+
+		for (RichNode leaf : leaves) {
+			RichNode parent = leaf.getParent();
+			if(parent != null) {
+				RichNode grandParent = parent.getParent();
+				if (grandParent != null && !grandParents.contains(parent)) {
+					leavesGrandParents.add(parent);
+					grandParents.add(parent);
+				}
+			}
+		}
+
+		return leavesGrandParents;
 	}
 
 }
