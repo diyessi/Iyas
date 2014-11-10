01fe444d467c71d78a15508fc7379d9e6491f07d
Massimo Nicosia <m.nicosia@gmail.com>
2014-11-10 16:05:36 +0100

    Removed wrong import

--- src/main/java/qa/qcri/qf/classifiers/SVMLightTKClassifierFactory.java
+++ src/main/java/qa/qcri/qf/classifiers/SVMLightTKClassifierFactory.java
@@ -1,13 +1,12 @@
 package qa.qcri.qf.classifiers;
 
 import svmlighttk.SVMLightTK;
-import svmlighttk.SVMLightTK_C;
 
 public class SVMLightTKClassifierFactory implements ClassifierFactory {
 
 	@Override
 	public Classifier createClassifier(String path) {
-		return new SVMLightTK_C(path);
+		return new SVMLightTK(path);
 	}
 
 }
