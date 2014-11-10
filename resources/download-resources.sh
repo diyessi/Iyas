f2a2dc47dd05a32328710c68997f0744df02dc44
Massimo Nicosia <m.nicosia@gmail.com>
2014-11-10 16:36:26 +0100

    Removed Limosine and TextPro

--- resources/download-resources.sh
+++ resources/download-resources.sh
@@ -1,5 +1,2 @@
-wget https://dl.dropboxusercontent.com/u/12803333/limosine.tar.gz
 wget http://alt.qcri.org/tools/ArabicPOSTaggerLib/APOSTLib.tgz
-wget https://www.dropbox.com/s/7qnxv4w5mdcmzzz/TextPro1.5.2_Linux64bit.zip
-
 wget http://crfpp.googlecode.com/files/CRF%2B%2B-0.58.tar.gz
