5f70128db415a44a0ca71899131b12e445feec14
Massimo Nicosia <m.nicosia@gmail.com>
2014-11-20 16:14:16 +0100

    Submission script for SVMLightTK

---
+++ semeval2015-3/svmlighttk/produce_submissions.py
@@ -0,0 +1,66 @@
+import sys
+import math
+
+A_LABELS = ["Not_English", "Good", "Potential", "Dialogue", "Bad"]
+
+B_LABELS = ["Yes", "No", "Unsure"]
+
+def main():
+	
+	'''
+		Subtask A
+	'''
+	
+	dir_prefix = "a/dev/"
+	
+	dev_cids = [line.split("|EV| #")[1].split("\t")[1].strip() for line in open(dir_prefix + "Bad.svm", "r")]
+	
+	preds = []
+	for label in A_LABELS:
+		preds.append([float(line.strip()) for line in open(dir_prefix + label + ".pred", "r")])
+		
+	with open("subtask_a.pred", "w") as out:
+		for prediction_index, predictions in enumerate(zip(*preds)):
+			label_index = predictions.index(max(predictions))
+			out.write(dev_cids[prediction_index] + "\t" + A_LABELS[label_index] + "\n")
+		
+	'''
+		Subtask B
+	'''
+	
+	dir_prefix = "b/dev/"
+	
+	dev_qids = [line.split("|EV| #")[1].split("\t")[0].strip() for line in open(dir_prefix + "Yes.svm", "r")]
+	
+	preds = []
+	for label in B_LABELS:
+		preds.append([float(line.strip()) for line in open(dir_prefix + label + ".pred", "r")])
+		
+	question_id_list = []
+	question_counter = {}
+		
+	for prediction_index, predictions in enumerate(zip(*preds)):
+		label_index = predictions.index(max(predictions))
+		question_id = dev_qids[prediction_index]
+		
+		if question_id not in question_counter:
+			question_id_list.append(question_id)
+			question_counter[question_id] = {"Yes": 0, "No": 0, "Unsure": 0}
+			
+		question_counter[question_id][B_LABELS[label_index]] += 1
+		
+	with open("subtask_b.pred", "w") as out:
+		for question_id in question_id_list:
+			
+			output_label = "Unsure"
+			
+			counter = question_counter[question_id]
+			if counter["Yes"] > counter["No"] and counter["Yes"] > counter["Unsure"]:
+				output_label = "Yes"
+			elif counter["No"] > counter["Yes"] and counter["No"] > counter["Unsure"]:
+				output_label = "No"
+			
+			out.write(question_id + "\t" + output_label + "\n")
+	
+if __name__ == "__main__":
+	main()
