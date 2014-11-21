ae51546345e41bec7ee33518ce14b84dcf33c355
Massimo Nicosia <m.nicosia@gmail.com>
2014-11-21 23:58:47 +0100

    Improved training/test scripts

--- semeval2015-3/subtask_ab.py
+++ semeval2015-3/subtask_ab.py
@@ -1,40 +1,36 @@
 import numpy as np
 import pandas as pd
 import sys
+import random
 
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.multiclass import OutputCodeClassifier
 from sklearn import svm
 from sklearn import linear_model as lm
 from sklearn import ensemble
 from sklearn import tree
 from sklearn import preprocessing
 from sklearn.utils import assert_all_finite
+from sklearn.metrics import f1_score
 from sklearn.grid_search import GridSearchCV
-
-SCALE = False
 
 RANDOM_STATE = 123
 
 def main():
+	
+	random.seed(RANDOM_STATE)
 	
 	if len(sys.argv) != 3:
 		print "Usage:", sys.argv[0], "<train file> <devel file>"
 		print "You will find the script output in the current directory."
 		sys.exit()
 	else:
-		subtask_a(sys.argv[1], sys.argv[2])	
+		#subtask_a(sys.argv[1], sys.argv[2])	
 		subtask_b(sys.argv[1], sys.argv[2])	
 
 def subtask_a(train, test):
 	train_ids, train_gold, train_data = read_data_subtask_a(train)
 	test_ids, test_gold, test_data = read_data_subtask_a(test)
 	
-	if SCALE:
-		ss = preprocessing.StandardScaler()
-		train_data = ss.transform_fit(train_data)
-		test_data = ss.transform(test_data)
-	
 	classifiers = []
 	
 	Cs = np.logspace(-5, 0, 11).tolist()
@@ -42,70 +38,160 @@ def subtask_a(train, test):
 	tolerances = [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3]
 	for c in Cs:
 		for tolerance in tolerances:
-			classifiers.append(svm.LinearSVC(C=c, tol=tolerance, random_state=RANDOM_STATE))
+			classifiers.append(OneVsRestClassifier(svm.LinearSVC(C=c, tol=tolerance, random_state=RANDOM_STATE)))
 
-	clf = GridSearchCV(estimator=OneVsRestClassifier(estimator=None), param_grid=dict(estimator=classifiers))
+	clf = gridSearchByField_a(classifiers, train_ids, train_gold, train_data, n_folds=5)
 	
 	clf.fit(train_data, train_gold)
 	predictions = clf.predict(test_data)
 	
-	print "Best parameters with score", clf.best_score_, ":"
-	print clf.best_params_
+	f1 = f1_score(test_gold, predictions, average="macro")
+	f1 = str(float("{0:2.2f}".format(f1 * 100)))
+	
+	print "Script F1:", f1 + "%"
 	
 	with open("subtask_a.pred", "w") as out:
 		for test_id, prediction in zip(test_ids, predictions):
 			out.write(str(test_id) + "\t" + str(prediction) + "\n")
 			
+def gridSearchByField_a(classifiers, train_ids, train_gold, train_data, n_folds=5):
+	ids = [id.split("_")[0] for id in train_ids]
+	unique_ids = list(set(ids))
+	random.shuffle(unique_ids)
+	
+	best_classifier = None
+	best_f1 = 0.0
+	
+	for clf in classifiers:
+		
+		f1s = []
+		
+		for fold_ids in slice_it(unique_ids, n_folds):
+			train_indexes = [id not in fold_ids for id in ids]
+			test_indexes = [id in fold_ids for id in ids]
+			
+			train = train_data[train_indexes]
+			train_labels = [label for label, include_it in zip(train_gold, train_indexes) if include_it]
+			
+			test = train_data[test_indexes]
+			test_labels = [label for label, include_it in zip(train_gold, test_indexes) if include_it]
+
+			clf.fit(train, train_labels)
+			predicted_labels = clf.predict(test)
+			f1 = f1_score(test_labels, predicted_labels, average="macro")
+			
+			f1s.append(f1)
+
+		mean_f1 = np.mean(f1s)
+		
+		if mean_f1 > best_f1:
+			best_classifier = clf
+			best_f1 = mean_f1
+			print best_classifier
+			print "CV F1: " + str(float("{0:2.2f}".format(best_f1 * 100))) + "%\n-\n"
+	
+	return best_classifier
+			
 def subtask_b(train, test):
 	train_ids, train_gold, train_data = read_data_subtask_b(train)
 	test_ids, test_gold, test_data = read_data_subtask_b(test)
-	
-	if SCALE:
-		ss = preprocessing.StandardScaler()
-		train_data = ss.transform_fit(train_data)
-		test_data = ss.transform(test_data)
 	
 	classifiers = []
 	
 	Cs = np.logspace(-5, 0, 11).tolist()
 	Cs.extend([1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3])
 	tolerances = [1e-3, 1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3]
+	#tolerances = [1]
 	for c in Cs:
 		for tolerance in tolerances:
-			classifiers.append(svm.LinearSVC(C=c, tol=tolerance, random_state=RANDOM_STATE))
+			classifiers.append(OneVsRestClassifier(svm.LinearSVC(C=c, tol=tolerance, random_state=RANDOM_STATE)))
 
-	clf = GridSearchCV(estimator=OneVsRestClassifier(estimator=None), param_grid=dict(estimator=classifiers), cv=5)
+	clf = gridSearchByField_b(classifiers, train_ids, train_gold, train_data, n_folds=5)
 	
 	clf.fit(train_data, train_gold)
 	predictions = clf.predict(test_data)
 	
-	print "Best parameters with score", clf.best_score_, ":"
-	print clf.best_params_
-	
 	question_id_list = []
 	question_counter = {}
+	question_gold = []
 	
-	for question_id in test_ids:
+	for index, question_id in enumerate(test_ids):
 		if question_id not in question_counter:
 			question_id_list.append(question_id)
 			question_counter[question_id] = {"Yes": 0, "No": 0, "Unsure": 0}
 	
 	for question_id, prediction in zip(test_ids, predictions):
 		question_counter[question_id][prediction] += 1
+		
+	question_gold = [line.strip().split("\t")[1] for line in \
+		open("data/SemEval2015-Task3-English-data/datasets/CQA-QL-devel-gold-yn.txt", "r")]
+		
+	output_labels = []
+	
+	for question_id in question_id_list:
+		output_label = "Unsure"
+			
+		counter = question_counter[question_id]
+		if counter["Yes"] > counter["No"] and counter["Yes"] > counter["Unsure"]:
+			output_label = "Yes"
+		elif counter["No"] > counter["Yes"] and counter["No"] > counter["Unsure"]:
+			output_label = "No"
+			
+		output_labels.append(output_label)
+		
+	f1 = f1_score(question_gold, output_labels, average="macro")
+	f1 = str(float("{0:2.2f}".format(f1 * 100)))
+	
+	print "Script F1:", f1 + "%"
 	
 	with open("subtask_b.pred", "w") as out:
-		for question_id in question_id_list:
-			
-			output_label = "Unsure"
-			
-			counter = question_counter[question_id]
-			if counter["Yes"] > counter["No"] and counter["Yes"] > counter["Unsure"]:
-				output_label = "Yes"
-			elif counter["No"] > counter["Yes"] and counter["No"] > counter["Unsure"]:
-				output_label = "No"
-			
+		for question_id, output_label in zip(question_id_list, output_labels):
 			out.write(question_id + "\t" + output_label + "\n")
+	
+def gridSearchByField_b(classifiers, train_ids, train_gold, train_data, n_folds=5):
+	ids = train_ids
+	unique_ids = list(set(ids))
+	random.shuffle(unique_ids)
+	
+	best_classifier = None
+	best_f1 = 0.0
+	
+	for clf in classifiers:
+		
+		f1s = []
+		
+		for fold_ids in slice_it(unique_ids, n_folds):
+			train_indexes = [id not in fold_ids for id in ids]
+			test_indexes = [id in fold_ids for id in ids]
+			
+			train = train_data[train_indexes]
+			train_labels = [label for label, include_it in zip(train_gold, train_indexes) if include_it]
+			
+			test = train_data[test_indexes]
+			test_labels = [label for label, include_it in zip(train_gold, test_indexes) if include_it]
 
+			clf.fit(train, train_labels)
+			predicted_labels = clf.predict(test)
+			f1 = f1_score(test_labels, predicted_labels, average="macro")
+			
+			f1s.append(f1)
+
+		mean_f1 = np.mean(f1s)
+		
+		if mean_f1 > best_f1:
+			best_classifier = clf
+			best_f1 = mean_f1
+			print best_classifier
+			print "CV F1: " + str(float("{0:2.2f}".format(best_f1 * 100))) + "%\n-\n"
+	
+	return best_classifier
+	
+def slice_it(li, cols=2):
+	start = 0
+	for i in xrange(cols):
+		stop = start + len(li[i::cols])
+		yield li[start:stop]
+		start = stop
 
 def read_data_subtask_a(data_file):
 	df = pd.read_csv(data_file)
--- semeval2015-3/task_arabic.py
+++ semeval2015-3/task_arabic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import sys
+import random
 
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn import svm
@@ -9,11 +10,14 @@ from sklearn import ensemble
 from sklearn import tree
 from sklearn import preprocessing
 from sklearn.utils import assert_all_finite
+from sklearn.metrics import f1_score
 from sklearn.grid_search import GridSearchCV
 
-SCALE = False
+RANDOM_STATE = 123
 
 def main():
+	
+	random.seed(RANDOM_STATE)
 	
 	if len(sys.argv) != 3:
 		print "Usage:", sys.argv[0], "<train file> <devel file>"
@@ -25,30 +29,73 @@ def main():
 def task(train, test):
 	train_ids, train_gold, train_data = read_data(train)
 	test_ids, test_gold, test_data = read_data(test)
-	
-	if SCALE:
-		ss = preprocessing.StandardScaler()
-		train_data = ss.transform_fit(train_data)
-		test_data = ss.transform(test_data)
 		
-	classifiers = [ensemble.AdaBoostClassifier(n_estimators=120)]
+	classifiers = []
 	
-	Cs = np.logspace(-5, 0, 11)
+	Cs = np.logspace(-5, 0, 11).tolist()
 	for c in Cs:
-		classifiers.append(svm.LinearSVC(C=c))
+		classifiers.append(OneVsRestClassifier(svm.LinearSVC(C=c)))
+	for n_estimators in [120]:
+		classifiers.append(OneVsRestClassifier(ensemble.AdaBoostClassifier(n_estimators=n_estimators)))
 
-	clf = GridSearchCV(estimator=OneVsRestClassifier(estimator=None), param_grid=dict(estimator=classifiers))
+	clf = gridSearchByField(classifiers, train_ids, train_gold, train_data, n_folds=5)
 	
 	clf.fit(train_data, train_gold)
+	predictions = clf.predict(test_data)	
 	
-	predictions = clf.predict(test_data)
+	f1 = f1_score(test_gold, predictions, average="macro")
+	f1 = str(float("{0:2.2f}".format(f1 * 100)))
 	
-	print "Best parameters with score", clf.best_score_, ":"
-	print clf.best_params_
+	print "Script F1:", f1 + "%"
 	
 	with open("task_arabic.pred", "w") as out:
 		for test_id, prediction in zip(test_ids, predictions):
 			out.write(str(test_id) + "\t" + str(prediction) + "\n")
+			
+def gridSearchByField(classifiers, train_ids, train_gold, train_data, n_folds=5):
+	ids = [id.split("-")[0] for id in train_ids]
+	unique_ids = list(set(ids))
+	random.shuffle(unique_ids)
+	
+	best_classifier = None
+	best_f1 = 0.0
+	
+	for clf in classifiers:
+		
+		f1s = []
+		
+		for fold_ids in slice_it(unique_ids, n_folds):
+			train_indexes = [id not in fold_ids for id in ids]
+			test_indexes = [id in fold_ids for id in ids]
+			
+			train = train_data[train_indexes]
+			train_labels = [label for label, include_it in zip(train_gold, train_indexes) if include_it]
+			
+			test = train_data[test_indexes]
+			test_labels = [label for label, include_it in zip(train_gold, test_indexes) if include_it]
+
+			clf.fit(train, train_labels)
+			predicted_labels = clf.predict(test)
+			f1 = f1_score(test_labels, predicted_labels, average="macro")
+			
+			f1s.append(f1)
+
+		mean_f1 = np.mean(f1s)
+		
+		if mean_f1 > best_f1:
+			best_classifier = clf
+			best_f1 = mean_f1
+			print best_classifier
+			print "CV F1: " + str(float("{0:2.2f}".format(best_f1 * 100))) + "%\n-\n"
+	
+	return best_classifier
+	
+def slice_it(li, cols=2):
+	start = 0
+	for i in xrange(cols):
+		stop = start + len(li[i::cols])
+		yield li[start:stop]
+		start = stop	
 
 def read_data(data_file):
 	df = pd.read_csv(data_file)
