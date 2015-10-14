40447a30e2d41bc4c8316f5eab343378a0e3d35c
Alberto Barron <albarron@gmail.com>
2015-10-14 10:26:21 +0300

    package for the Semeval 2016 code

---
+++ src/main/java/qa/qf/qcri/semeval2016_3/XmlParser.java
@@ -0,0 +1,13 @@
+package qa.qf.qcri.semeval2016_3;
+
+/**
+ * This class reads the SemEval 2016 task 3 on cQA XML input file and makes an
+ * object available with the questions and threads.  
+ * @author albarron
+ *
+ */
+public class XmlParser {
+
+  
+  
+}
